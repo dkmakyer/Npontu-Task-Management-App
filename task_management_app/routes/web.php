23f2ec9f264519395164c04081046dc0af5dd3ab
Sammy <gbedysamuel@gmail.com>
2025-03-08 15:57:07 +0000

    added descriptive comments

--- task_management_app/routes/web.php
+++ task_management_app/routes/web.php
@@ -12,12 +12,19 @@ Route::get('/', [LandingPageController::class, 'index'])->name('home');
 Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::post('login', [LoginController::class, 'store']);
 
-Route::get('auth/redirect/{social}', [SocialiteController::class, 'redirectLogin'])->name('social.login');
-Route::get('auth/callback/{social}', [SocialiteController::class, 'getSocialUser']);
+/**
+ * routes to serve the request to login to the app using any provider
+ * provider parameter refers to the name of the login provider
+ * to make the route dynamic
+ */
+Route::get('auth/redirect/{provider}', [SocialiteController::class, 'providerLogin'])->name('social.login');
+Route::get('auth/callback/{provider}', [SocialiteController::class, 'providerUser']);
 
 Route::get('register', [RegisterController::class, 'index'])->name('register');
 Route::post('register', [RegisterController::class, 'store']);
 
-// used a get method for testing because logout button wasnt created
-// Will later switch to post after its been created 
+/**
+ * used a get method for testing because logout button wasnt created
+ * Will later switch to post after its been created 
+ */
 Route::get('logout', [LogoutController::class, 'logout'])->name('logout');
