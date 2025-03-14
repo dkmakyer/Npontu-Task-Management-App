93c338fa4370060ed28d05e27ec1eb854a82b5d8
Mawuli Agbeko <jmmyagbeko@st.ug.edu.gh>
2025-03-13 14:24:37 -1200

    sidebar changes

--- frontend-files/All tasks/all-tasks.js
+++ frontend-files/All tasks/all-tasks.js
@@ -18,3 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+//notification popup
+document.getElementById('notificationButton').addEventListener('click', function() {
+    document.getElementById('notificationPopup').classList.remove('hidden');
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('notificationPopup').classList.add('hidden');
+});
+
+document.getElementById('notificationPopup').addEventListener('click', function(event) {
+    if (event.target === this) {
+        document.getElementById('notificationPopup').classList.add('hidden');
+    }
+});
